d3b9c9a8f26440f7f58f0e282c174f643bade947
DevendraHolikar <devendraholikar@gmail.com>
2025-12-30 23:17:24 +0530

    removed firebase API

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5rgYt0MW-Bw4dDbiewtkKDhAymh_Ryx4",
-  authDomain: "netflix-gpt-31e3c.firebaseapp.com",
-  projectId: "netflix-gpt-31e3c",
-  storageBucket: "netflix-gpt-31e3c.firebasestorage.app",
-  messagingSenderId: "891096850170",
-  appId: "1:891096850170:web:52e91660c9e641653df343"
+
 };
 
 const app = initializeApp(firebaseConfig);
